eb09358772c22abd786edf79ad9e709950cac736
amir <amirtaki623@gmail.com>
2025-05-06 14:21:21 +0330

    Refernce Types 3- function type

--- src/script.js
+++ src/script.js
@@ -2,41 +2,19 @@ console.log('javaScript Master');
 
 
 // Refernce Types
-// 2- Array type
+// 3- function type  
 
-let users = ["amir", "mani"]
-console.log(users)
-console.log(typeof(users))
+function greet (name, age){
+    console.log("hello world!!");
+    console.log(name , age);
+}
+function clac (numberOne, numberTwo){
+  return numberOne + numberTwo
+}
 
-console.log(users[0]);
-console.log(users[1]);
-console.log(users[2]);
+greet("amir", 28);
 
-users[2] = "javaScript";
-
-console.log(users[2]);
-
-users[3] = 123;
-
-users[4] = false;
-
-console.log(users[3])
-
-console.log(users[4])
+console.log(clac(1000, 2000));
 
 
-
-console.log(typeof(users))
-console.log(typeof users)
-// نکته در واقع آرایه ها نوعی آبجکت هستند
-//  ['amir', 'mani']
-// 0: "amir"
-// 1: "mani"
-// 2: "javaScript"
-// 3: 123
-// 4: falselength: 
-// 5[[Prototype]]: Array(0)
-
-console.log(users.length)
-
-document.getElementById("text").innerHTML = " Refernce Types : 2- Array type";
+document.getElementById("text").innerHTML = " Refernce Types : 3- function type";
