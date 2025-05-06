d098b7a833edc702874a095c0acfa6fa88eca3dc
amir <amirtaki623@gmail.com>
2025-05-06 20:49:59 +0330

    factory function in JS

--- src/script.js
+++ src/script.js
@@ -1,41 +1,54 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = " Objects";
+document.getElementById("text").innerHTML = " factory function";
 
-// Objects
-
-
-// let radius = 1;
-// let x = 1;
-// let y = 1;
+// factory function
 
 
-// function draw (){
-
-// }
-
-
-// Object oriented programming
 let cirle = {
-    radius : 1, 
+    radius : 2,
     location : {
-        x : 1, 
-        y : 1
+        x : 2, 
+        y : 2,
     },
-    isVisible : true,
-    draw : function(){console.log('draw')} // method
-};
+    isVisibble : true,
+    draw : function() {console.log("draw")}
+}
+
+// Factory function
+function createCircle (randius, location, check ){
+    return {
+        
+            radius : randius,
+            location : location,
+            isVisibble : check,
+            draw : function() {console.log("draw")}
+        
+    }
+}
+
+let obj = createCircle(1, {x : 1, y : 3}, true);
+console.log(obj)
+console.log(obj.draw())
 
 
-cirle.draw();
+// syntax new
+// اکر نوشتاری کلید و مقدار یکی باشد مینوانیم یک بار بنویسیم
+//  همچنین برای متد هم میتوان از نوشتن کلمه فانکشن خودداری کرد
+
+function createCircle2 (radius, location, isVisibble){
+    return {
+        radius, 
+        location, 
+        isVisibble,
+        draw() {console.log("create-draw")}
+    }
+}
 
 
+let objectCircle  = createCircle2(1, {x : 12, y :15}, false)
+console.log(objectCircle)
+console.log(objectCircle.draw())
 
-//      let objectName = {
-//          key : value property,
-//          objectName : {
-//              key : value
-//           },
-//           method :fucntion () {} 
-// }
-
-
+let objectCircle2 = createCircle2(2, {x : 15 ,y: 20},true)
+console.log(objectCircle2)
+console.log(objectCircle2.draw())
