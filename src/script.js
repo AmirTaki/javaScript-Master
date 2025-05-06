5fbd9557eaa69c16882d5ee7921959214a47c159
amir <amirtaki623@gmail.com>
2025-05-06 23:19:40 +0330

    object math in JS

--- src/script.js
+++ src/script.js
@@ -1,5 +1,27 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = "- garbage collection";
+document.getElementById("text").innerHTML = "- object math";
 
 
-// -garbage collection
+// -object math
+
+console.log(Math.PI)
+
+console.log(Math.random())
+
+
+function getRandomArbitray(min, max){
+    return Math.floor(Math.random() * (max-min) + min);
+}
+
+console.log(getRandomArbitray(1, 1000))
+
+console.log(Math.round(1.5))
+
+console.log(Math.round(2.9))
+
+console.log(Math.max(2, 9, 4, 645))
+
+console.log(Math.min(2, 9, 4, -645))
+
+console.log(Math.abs(-5))
+
