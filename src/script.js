48d0be5fb68081877e20c57d114feec7959477eb
amir <amirtaki623@gmail.com>
2025-05-06 23:44:57 +0330

    template literal in JS

--- src/script.js
+++ src/script.js
@@ -1,62 +1,28 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = "- object string";
+document.getElementById("text").innerHTML = "template literal";
+
+//template literal
+
+const message = "\'JavaScript\' . \n '' coding is cool" 
 
 
-// -object string
+// literal 
+// ubject = {}
+// bollean = true, false
+// string ' "
+// string `
 
+const text = ` 'JavaScript' .
+coodin is cool
+`;
 
-
-//ما در اسکریپت دو نوع آبجکت داریم 
-
-// 1-string primive
-
-const message = "javaScript cooding is cool"
-
-// 2- string object
-const another = new String(message)
-
-
-console.log(typeof(message))
-
-console.log(typeof(another))
-
-console.log(message)
-// message. => convert primive to object
-
-console.log(message.length)
-
-console.log(message[3]);
-
-console.log(message.includes('is'));
-
-console.log(message.startsWith('co'));
-
-console.log(message.endsWith('cool'));
-
-console.log(message.indexOf("coo"));
-
-console.log(message.indexOf("m"));
-
-console.log(message.replace("cooding","code"));
-
-console.log(message.toLowerCase());
-
-console.log(message.toUpperCase());
-
-const mess = "  javaScript cooding is cool   " 
-
-console.log(mess.trim())
-
-console.log(mess.trimLeft())
-
-console.log(mess.trimRight())
-
-
-const text = 'javaScript cooding\n is \"cool\" alpha   '
 console.log(text)
 
-console.log(mess.split(' '))
 
-console.log(mess.split('a'))
+let name = "amir"
 
-console.log('java-Script-cooding-is-cool-'.split('-'))
+const msg = "hi " + name  +". thank you"
+console.log(msg)
+
+
+console.log(`hi ${name} . thank you`)
