43840829957af285b0dd3694b8b90618f563097e
amir <amirtaki623@gmail.com>
2025-05-06 18:13:05 +0330

    -Bitwise operators

--- src/script.js
+++ src/script.js
@@ -1,29 +1,16 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = "  -Logical operators on non-Booleans";
+document.getElementById("text").innerHTML = "  -Bitwise operators";
 
+// Bitwise operators
+// 0 1
 
-//-Logical operators on non-Booleans"
+// 00000001 = 1
+// 00000010 = 2
+// _____________
+// 00000011 = 3
+// 00000000 = 0
+// bitwise AND : &
+// bitwise OR  : |
 
-// false => false, undifind,, null, 0 , "", NaN
-
-// ture => true, 0, -1, 1, "string", ......
-
-console.log(false || true)
-
-console.log(false || "test")
-
-console.log(false || 0 || true)
-
-console.log(false || 1 || false) // => (false || true || false) => true
-
-
-console.log(false && true)
-
-console.log(false && "test")
-
-console.log(true && 0 && "test") // => (ture && false && true) => false:0
-
-console.log(true && 1 && "7" || false && "test")
-// true && true &&  true => True(7)
-// false && true(7) => false
-// true(7) || false => True(7)
+console.log(1 | 2)
+console.log(1 & 2)
