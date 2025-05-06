c35c9046db74d6c1825468cfd4cf9654199349c8
amir <amirtaki623@gmail.com>
2025-05-06 14:28:56 +0330

    Arthmetic Operators

--- src/script.js
+++ src/script.js
@@ -1,20 +1,33 @@
 console.log('javaScript Master');
 
 
-// Refernce Types
-// 3- function type  
+// Arithmetic operators
+let x = 10;
+let y = 5;
 
-function greet (name, age){
-    console.log("hello world!!");
-    console.log(name , age);
-}
-function clac (numberOne, numberTwo){
-  return numberOne + numberTwo
-}
+console.log(x + y)
 
-greet("amir", 28);
+console.log(x - y)
 
-console.log(clac(1000, 2000));
+console.log(x / y)
+
+console.log(x * y)
+
+console.log(x % y) // باقی مانده تقسیم
+
+console.log(x ** y)
+
+console.log(x++)
+
+console.log(x)
+
+console.log(++x)
+
+console.log(y--)
+
+console.log(y)
+
+console.log(--y)
 
 
-document.getElementById("text").innerHTML = " Refernce Types : 3- function type";
+document.getElementById("text").innerHTML = "  Arithmetic operators";
