319a6513eae34baf97eac02df4b85067197a0963
amir <amirtaki623@gmail.com>
2025-05-06 13:27:23 +0330

    Variables in JS

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
 console.log('javaScript Master');
+// Variables
+
+let name  = "javaSctipt";
+console.log (name)
+
+
+
+// قوانین نام گذاری
+// از اسم کلیدی نمیتوان  برای نام کذاری انتخاب کرد  
+// let, if, ......
+
+let userName = "amir";
+console.log(userName);
+
+let lastName = "taki";
+console.log(lastName);
+
+
+document.getElementById("text").innerHTML = userName +  " "+ lastName;
