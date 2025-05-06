8d540f169fa7dfac19a32490b9a00ebeea1f0f9e
amir <amirtaki623@gmail.com>
2025-05-06 21:32:12 +0330

    Constructor Property => Construcotr Litral in JS

--- src/script.js
+++ src/script.js
@@ -1,24 +1,56 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = " - Objects are dynamic";
+document.getElementById("text").innerHTML = " Constructor Property";
 
 
 
+// Construcotr Litral
+// - Constructor Property
 
-// - Objects are dynamic
+// Factory Function 
 
-const cirlce = {
-    radius : 1   
+function createCircle (radius){
+    return {
+        radius, 
+        draw() {console.log("Factory Function")}
+    }
 }
 
-cirlce.color = "red";
-cirlce.draw = function (){}
+const circle = createCircle(1);
 
-console.log(cirlce)
+// Constaructor Fucntion
+function Circle (radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log("Constractor function")
+    }
+}
 
-delete cirlce.radius;
+const another = new Circle(2);
 
-delete cirlce.draw;
+console.log(circle.constructor)
+console.log(another.constructor)
 
-console.log(cirlce)
+//  وقتی برای ساختن آبجکت از کانسترکتور خودمان بسازیم اگر نسازیم خود جاوااسکریپت داخل خودش از کانسکتراکتور درونی خودش استفاده مبکند.
+
+let x = {};
+console.log(x.constructor)
 
 
+// let x = new Object() ;
+
+
+// new String(); => new String ("javaScript")
+
+let name = "JavaScript";
+console.log(name.constructor)
+
+// new Boolean(); => new Boolean (true)
+let bool = true;
+console.log(bool.constructor)
+
+// new number(); => new number (1)
+let number = 1;
+console.log(number.constructor)
+
+
+// هر آبجکت  یک پراپرتی کانستراکتور دارد که به فانکشنی اشاره میکند که اون آبجکت به وجود آورده است
