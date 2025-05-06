0a695bfb4bfd946d3546126b9bfe3a6fa4e92236
amir <amirtaki623@gmail.com>
2025-05-06 23:12:37 +0330

    garbage collection in JS

--- src/script.js
+++ src/script.js
@@ -1,32 +1,5 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = "- Clone an object";
+document.getElementById("text").innerHTML = "- garbage collection";
 
 
-// - Clone an object
-
-const circle = {
-    radius : 1, 
-    draw() {
-        console.log("draw")
-    }
-}
-
-const another = circle;
-
-// راه حل اول
-const anotherOne = {}
-
-for (let key in circle){
-    anotherOne[key] = circle[key]
-    
-}
-console.log(anotherOne)
-
-// راه حل دوم
-const anotherTwo = Object.assign({color: "black"}, circle)
-
-console.log(anotherTwo)
-
-// راه حل سوم
-const anotherThree = {location : "usa",color:"red", ...circle}
-console.log(anotherThree)
+// -garbage collection
