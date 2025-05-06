7e549dc2ba3e27602dea6644d2f4fd7c57773cc1
amir <amirtaki623@gmail.com>
2025-05-07 02:56:55 +0330

    Arrow Function

--- src/script.js
+++ src/script.js
@@ -1,43 +1,20 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = " - Finding an element within an array";
+document.getElementById("text").innerHTML = " - Arrow Function";
 
-// - Finding an element within an array
+// Arrow Function
 
-// 1- primitve
-const numbers = [1, 2, 3, 4, 5, 1]
-
-console.log(numbers.indexOf(1))
-
-console.log(numbers.lastIndexOf(1))
-
-if(numbers.indexOf(7) !== -1){
-    console.log('found')
-}
-
-console.log(numbers.includes(8))
-
-console.log(numbers.indexOf(1, 4))
-
-
-// 2- reference
-
-const numbersObj = [
-    {id : 1, name :"a"},
+const course = [
+    {id : 1, name : "a"},
     {id : 2, name : "b"}
 ];
 
-console.log(numbersObj.includes({id : 1, name: 'a'}))  // false
+// const result = course.find(function(course){
+//     return course.name === "a";
+// })    
+// console.log(result)
 
-// console.log(numbersObj.find())
 
-const result = numbersObj.find(function(number){
-    return number.name === "b"
-})
+const result = course.find((course)=>course.name === "a")
 
 console.log(result)
 
-const resutlIndex = numbersObj.findIndex(function(number){
-    return number.name === "b"
-})
-
-console.log(resutlIndex)
