efd130a2c55c231c8b76e9c37dcc9270532de740
amir <amirtaki623@gmail.com>
2025-05-06 21:12:44 +0330

    - Objects are dynamic in JS

--- src/script.js
+++ src/script.js
@@ -1,61 +1,24 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = " constructor function";
+document.getElementById("text").innerHTML = " - Objects are dynamic";
 
 
 
 
-// Factory Function
+// - Objects are dynamic
 
-function createCircle (radius, location, isVisibble){
-    return {
-        radius, 
-        location, 
-        isVisibble,
-        draw() {console.log("create-draw")}
-    }
+const cirlce = {
+    radius : 1   
 }
 
-const myCircle = createCircle(1, {x : 12, y : 13}, true)
+cirlce.color = "red";
+cirlce.draw = function (){}
 
-// constructor function
+console.log(cirlce)
 
-function Circle(radius, location, isVisibble){
-    this.radius  = radius,
-    this.location = location,
-    this.isVisibble = isVisibble;
-    this.draw = function(){
-        console.log ("draw")
-    }
-    //return this;  // نیازی نیست اینو بنوسیبم توسط اپریتور نیو انجام میشود
-}
+delete cirlce.radius;
 
-const circle = new Circle(3, {x : 4,  y: 2 }, false)  ;
+delete cirlce.draw;
 
-console.log(circle)
+console.log(cirlce)
 
 
-
-
-// اپریتور new :
-// یک آبجکت خالی ایجاد میکند
-
-// اپریتور یا کلمه this
-// به آبجکت خالی اشاره میکند
-
-// آبجکت مقدار دهی شده برمیگرداند
-
-
-// camel notation : oneTwoThree
-// pascal notaion : OneTwoThree
-
-
-//  تفاوت بین factory function & constarctor function
-// در فکتوری فانکشن ما یک متغیر میساختیم واسم آبجکت میدادیم و  اتفاقی که می افتد آبجکت برای ما بر میگرداند
-
-// اما در conastractor function
-// اپریتور نیو میساختیم ویک آبجکت میسازیم و چیزی درون آبجکت
-// return
-// نمیکنیم
-// و از کلمه کلیدی
-// this
-// استفاده میکنیم
