154ba98366fbed621ace2b1605351d619e5a40a5
amir <amirtaki623@gmail.com>
2025-05-07 19:23:35 +0330

    Sorting arrays in JS

--- src/script.js
+++ src/script.js
@@ -1,28 +1,32 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = "- Array join";
+document.getElementById("text").innerHTML = "- Sorting arrays"
 
-/// - Array join
+/// - - Sorting arrays
 
-const numbers = [1, 2, 3, 4, 5, 6, 7] 
+const numbers = [6, 1, 16, 2, 19, 3, 4, -4]
 
-const joined = numbers.join();
+numbers.sort();
 
-console.log(joined)
+console.log(numbers)
 
-const joinedNew = numbers.join('-')
+numbers.reverse();
 
-console.log(joinedNew)
+console.log(numbers)
 
-console.log(typeof(joinedNew))
+const obj = [
+    {   id : 1, name : 'html'},
+    {   id : 2, name : 'css'},
+    {   id : 3, name : 'javaScript'},
+    {   id : 4, name : 'react'},
+    {   id : 4, name : 'nextjs'},
+]
 
+obj.sort(function(a, b){
+    let nameA = a.name.toLocaleLowerCase()
+    let nameB = b.name.toLocaleLowerCase()
+    if (nameA < nameB) return -1;
+    if (nameB > nameA) return 1;
+    return 0
+})
 
-const message = "Array Join in JavaScript";
-
-const parts = message.split(" ")
-console.log(parts)
-
-const combiend = parts.join('-')
-
-console.log(combiend)
-
-
+console.log(obj)
