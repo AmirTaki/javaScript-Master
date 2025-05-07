4df84c8b77561084a375f70d5691542dbc5fc0a1
amir <amirtaki623@gmail.com>
2025-05-07 21:19:15 +0330

    Reduce method in array

--- src/script.js
+++ src/script.js
@@ -1,22 +1,25 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = "-Arguments in JS"
+document.getElementById("text").innerHTML = "Reduce method in array"
 
-// - Arguments
+// -Reduce method in array
 
-function sum(a, b){
-    // console.log(arguments)
-    let total = 0;
-    for (let value of arguments){
-        total += value;
-    }
-    return total
-}
+const numbers = [1, -1, 5, 18, 54]
+let sum = 0;
 
-console.log(sum(1, 3))
-console.log(sum())
-console.log(sum(1, ))
-console.log(sum(1, 2, 3, 4 ))
+for(let n of numbers) sum+= n
+console.log(sum)
 
+let Reduce = numbers.reduce((accumulator, currentValue )=>{
+    // console.log(accumulator, currentValue)
+    return accumulator + currentValue;
+}, 0)
 
-// اگرایتریتور نداشته باشد نمینوان استفاده کرد
-
+console.log(Reduce)
+/*
+ accumulator = 0 , currentValue = 1 : 1
+ accumulator = 1, currentValue = -1 : 0
+ accumulator = 0, currentValue = 5 : 5
+ accumulator = 5, currentValue = 18 : 24
+ accumulator = 24, currentValue = 54 : 77
+ accumulator = 77 : 77
+*/
