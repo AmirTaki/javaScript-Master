45dd6c0bfd53719898d01d781772d26a70c75a59
amir <amirtaki623@gmail.com>
2025-05-07 21:02:45 +0330

    Hoisting in JavaScript

--- src/script.js
+++ src/script.js
@@ -1,98 +1,41 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = "- - - What 'this' does"
+document.getElementById("text").innerHTML = "-Hosting in JS"
 
-// - this
-
-// function in object => method
-// method in object => this
-
-// functiion normall => this : global object
-// global object in browser ---> window
-// global object in node  -----> global
+// - Hosting
 
 
-// this in function -> global (browser : windonw, node : global)
-
-//-----------------------------------   -------------------------------
-
-// OBJECT
-const video = {
-    title : "a",
-    play() {
-        console.log(this)
-    },
-    stop() {
-        console.log(this)
+// FUNCTION Declaration
+walk()
+function walk () {
+        console.log("walk")        
     }
-}
-
-video.play()
-
-// this
-//   درون یک متد هست و آبجکت ما درون فانکشن است بنابراین 
-// this
-// به آبجکتی که آن متد در آن قرار دارد اشاره میکند
-
-video.stop()
-
-//-----------------------------------   -------------------------------
-// FUNCTION
-
-function playVideo() {
-    console.log(this);
-}
-
-playVideo() // window
-// یک فانکشن است باید کلوبال برای ما نمایش دهد
-
-//-----------------------------------   -------------------------------
-// CONSTRANCTOR FUNCTION
-
-function Videos (title) {
-    this.title = title;
-    console.log(this)
-
-    // return this
-}
-
-const V = new Videos('practice')
-
-// ویندو برای ما لاگ نمیگیرد چرا ؟
-
-// وقتی از کلمه
-// new 
-// استفاده میکنیم 3 کار انجام میدهد
-// 1)
-// ابتدا یک آبجکت خالی جدید میسازد
-// 2)
-//this
-// به آبجکت خالی جدید اشاره مبکند
-// 3)
-// در نهایت کلمه 
-//  this 
-// برای ما بر میگرداند
-
-//-----------------------------------   -------------------------------
-
-const film = {
-    title: 'film:',
-    tags : ['a', 'b', 'c'],
-    showTags(){
-        this.tags.forEach(e => {
-            console.log(this.title, e)
-        })
-    },
-    
-    showTagsF(){
-        this.tags.forEach(function(e)  {
-            console.log(this.title, e)
-        },this)
-    }
-}
 
 
-film.showTags()
-console.log("----------------")
-film.showTagsF()
+// FUNCTION  Expression
 
-console.log('a', 'b')
+// run ()
+//  Error : Warning Uncaught ReferenceError ReferenceError: Cannot access 'run' before initialization
+
+let run = function () {
+    console.log("run")
+};
+walk()
+run()
+
+
+// تفاوت کلیدی 
+// FUNCTION Declaration در
+// میتوان قبل از تعریف شدن آن آن را صدا زد
+//امابا  FUNCTION  Expression
+// نمیتوان آن را صدا زد
+
+
+// دلیل اجرا این است موتور جاوااسکریپت قبل از اجرا 
+// function declaration
+// به بالا تر از بقیه اجرا میکند
+// و به این عملکرد 
+//  hoistiog
+// میگویند
+
+let move = run;
+move();
