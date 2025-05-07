bdc7a24c79fbe02c78cd126ea1a1d1f4f067c7a3
amir <amirtaki623@gmail.com>
2025-05-07 19:38:31 +0330

    Filtering an array

--- src/script.js
+++ src/script.js
@@ -1,19 +1,25 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = "Testing array elements"
+document.getElementById("text").innerHTML = "- Filtering an array"
+
+// - Filtering an array
+
+const numbers = [1, 10, 12, 4, -8, -9, -18, 22, 39]
 
 
-// - Testing array elements
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-const allPositive = numbers.every((value)=>{
+const array = numbers.filter((value)=>{
     return value >= 0
 })
 
-console.log(allPositive)
+console.log(array)
 
-const atLeastOnePosive = numbers.some((value)=>{
-    return value >= 20
-})
 
-console.log(atLeastOnePosive)
+const users = [
+    {name : "amir", age : 28},
+    {name : "mani", age : 20},
+    {name : "nia", age :  25 },
+    {name : "kia", age : 14},
+    {name : "reza", age : 16},
+]
+const result =  users.filter(user => user.age >= 18) 
+
+console.log(result)
