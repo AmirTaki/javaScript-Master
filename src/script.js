ec7da0992deae7bb1f610c8a54abc174e16d55fd
amir <amirtaki623@gmail.com>
2025-05-08 22:59:44 +0330

    Member Itrate prototype vs instance member oop

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML =  "prototype vs instance member oop"
+document.getElementById("text").innerHTML =  " Member Itrate prototype vs instance member oop"
 
-//  - prototype vs instance member Prototypobject-oriented programming
+//  -Member navigation prototype vs instance member Prototypobject-oriented programming
 
 
 // Constractor function
@@ -13,89 +13,33 @@ function Circle(radius){
         console.log("move")
     }
  
-    /* this.draw = ()=>{
-        console.log('draw')
-    }*/
 };
+
 
 // object => constarctor function
 const  c1 = new Circle (1)
-const  c2 = new Circle (2)
-/*
- هر 
- constranctor 
-یک پروتایپ دارد 
-که مقدار آن با
-__proto__
-هر ابجکت برابر است
-*/
 
 
-//به طریق زیر میتوان هر متد یا پراپرتی داخل پروتایپ هر کانیستراکتور فانکشن قرار داد
-// 2- prototype members
-
-Circle.prototype.draw =function(){
-    this.move();
-    console.log("draw")
+// 2-prototype members
+Circle.prototype.draw = function () {
+    console.log('draw')
 }
 
-console.log(c1)
+
 c1.draw()
-c2.draw()
-
-console.log(c1.__proto__) 
-
-/*
-در جاوا اسکریپتت به طور کلی دو نوع پراپرتی و متد داریم
-1- Instance members
-2- prototype members
-*/
+c1.move()
 
 
+//  returns instance members
+console.log(Object.keys(c1))
 
 
-Circle.prototype.toString = function(){
-    return "circle with radius :" + this.radius;
+// returns all memebers => instance members & prototype members
+for(let key in c1){
+    console.log(key)
 }
 
-console.log(c1.toString())
+console.log(c1.hasOwnProperty('radius')) // true => instance members
 
+console.log(c1.hasOwnProperty('draw'))  // false => property members
 
-console.log('------------------------------------------------')
-
-function Circle2(radius) {
-    // instace members 
-    this.radius = radius;
-    this.move = function () {
-        this.draw();
-        console.log("move")
-    }
-}
-
-
-// prototype members
-Circle2.prototype.draw = function() {
-    console.log("draw");
-}
-
-
-const objC1 = new Circle2(12)
-
-console.log(objC1)
-
-objC1.move()
-
-
-/* 
-هم در 
-instance members 
-میتوان 
-protototype members
-صدا زد و 
-هم در 
-prototype members
-میتوان 
-instance members
-صدا زد
-
-*/
