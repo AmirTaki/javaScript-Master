6bb1f83f77873b0dede2058c755389cc73f41398
amir <amirtaki623@gmail.com>
2025-05-08 21:46:29 +0330

    Property description oop

--- src/script.js
+++ src/script.js
@@ -1,82 +1,33 @@
 console.log('javaScript Master');
-document.getElementById("text").innerHTML = "-Multilevel inheritance oop"
+document.getElementById("text").innerHTML =  "Property description oop"
 
-//  Multilevel inheritance object-oriented programming
-let myArray = []
-console.log([])
-
-function Circle(radius){
-    this.radius = radius,
-    this.draw =()=> {
-        console.log('draw')
-    }
-}
-
-const circle = new Circle(10)
-
-console.log(circle)
+//  - Property description object-oriented programming
+let person = {name : "amir"}
+console.log(person)
 
 
-const circle2 = new Circle(12)
+// new Object()
+
+console.log(person.toString())
+
+for (let key in person) console.log(key, person[key])
+
+console.log(Object.keys(person))
+
+//ابجکت ما یک سری پراپرتی دارد و هر پراپرتی یک سری اتربیوت دارد  که به ان ملحق شدند
+
+let objectBase = Object.getPrototypeOf(person)
+let descriptor = Object.getOwnPropertyDescriptor(objectBase, 'toString')
+console.log(descriptor)
 
 
-console.log(circle2)
+Object.defineProperty(person,'name',{
+    writable: false ,   //   // تغییر ندادن مقدار پراپرتی
+    
+    enumerable  : false  // پیمایش نکردن
+});
 
+person.name = "mani"
+console.log(person)
 
-/*
-آبجکت پرتوتایپ ویژه همه ابجکت هایی که به وسیله ی این 
-canstarctor Function circle
-ایجادشدند
-
-اسم این بخش بگذاریم  سرکل بیس :Circle base
-
-
-Circle {radius: 10, draw: ƒ}
-script.js:17
-draw =
-()=> {\r\n        console.log('draw')\r\n    }
-radius =
-10
-[[Prototype]] =
-Object
-constructor =
-ƒ Circle(radius){\r\n    this.radius = radius,\r\n    this.draw =()=> {\r\n        console.log('draw')\r\n    }\r\n}
-arguments =
-null
-caller =
-null
-length =
-1
-name =
-'Circle'
-prototype =
-{}
-[[FunctionLocation]] =
-@ c:\Users\ASUS\Desktop\backup _ toturial\Js-master\src\script.js:8
-[[Prototype]] =
-ƒ ()
-[[Scopes]] =
-Scopes[2]
-[[Prototype]] =
-Object
-  
-*/
-
-/*
-یعنی هر موقع که ما یک 
-constractor
-که یک ابجکت صدا میزنیم 
- */
-
-/*  
-همه ابجکت های که کانستراکتور  یکسانی دارند 
-پرتوتایپ یکسانی دارند
-*/
-
-let array2 = [2]
-let array3 = [2]
-
-console.log(array2)
-console.log(array3)
-
-// کانسکتور جفت آرایه یکسان است
+console.log(Object.keys(person))
