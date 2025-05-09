b6e62738a2299fcf0e6e6612c4a77309992513e6
amir <amirtaki623@gmail.com>
2025-05-09 17:28:09 +0330

    Asyncronous CallBack in JS

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-document.getElementById("text").innerHTML =  "- Asyncronous VS Syncronous in JS";
+document.getElementById("text").innerHTML =  "- Asyncronous CallBack in JS";
 
 /*
-Asyncronous VS Syncronous in JavaScript :
-
-Asynconous :
-1- call back
-2- promise
-3- Async and Await
-
+Asyncronous CallBack in JavaScript :
 */
+
+function ball1(callback){
+    setTimeout(()=>{
+          console.log("the ball one reached the end of the hill");
+          callback(ball3);
+    },2000)
+  
+}
+function ball2(callback){
+    setTimeout(()=>{
+          console.log("the ball two reached the end of the hill");
+          callback();
+    },1000)
+  
+}
+function ball3(){
+    setTimeout(()=>{
+          console.log("the ball three reached the end of the hill");
+    },3000)
+  
+}
+
+// ball1();
+// ball2();
+// ball3();
+
+
+ball1(ball2)
+
