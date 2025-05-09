ba364f5a7bd049148f2ddbc061bb1e96f1ec5583
amir <amirtaki623@gmail.com>
2025-05-09 19:39:06 +0330

    Asyncronous : Promise   in JS

--- index.js
+++ index.js
@@ -1,118 +1,47 @@
-document.getElementById("text").innerHTML =  "- Managing errors in callbacks in JS";
+document.getElementById("text").innerHTML =  "- Asyncronous : Promise   in JS";
 
 /*
-Asyncronous : Managing errors in callbacks in JavaScript :
+Asyncronous : Promise in callbacks in JavaScript :
 */
 
-
-
-/*
-function ball1(ball2){
-    setTimeout(()=>{
-        try{
-            if (true){
-                console.log("the ball one reached the end of the hill");
-                ball2(ball3);
-            }
-            else {
-                throw new Eror ("the ball one failed")
-            }
-
+function ball1(){
+    return new Promise((resolve, reject)=>{
+        if(true){
+            resolve("the ball one reached the end of the hill");
         }
-        catch(error){
-            console.log(error)
+        else {
+            reject("the ball one failed");
         }
-    },2000)
-  
+    } )
 }
-function ball2(callback){
-    setTimeout(()=>{
-       try{
-            if (false){
-                console.log("the ball two reached the end of the hill");
-                callback();
-            }
-            else {
-                throw new Eror ("the ball two failed")
-            }
-
+function ball2(){
+    return new Promise((resolve, reject)=>{
+        if(true){
+            resolve("the ball two reached the end of the hill");
         }
-        catch(error){
-            console.log(error)
+        else {
+            reject("the ball two failed");
         }
-    },1000)
-  
+    } )
 }
 function ball3(){
-    setTimeout(()=>{
-          console.log("the ball three reached the end of the hill");
-    },3000)
-  
+    return new Promise((resolve, reject)=>{
+        if(false){
+            resolve("the ball three reached the end of the hill");
+        }
+        else {
+            reject("the ball three failed");
+        }
+    } )
 }
-
-ball1(ball2)
-
-*/
-
-function ball1(callback){
-    setTimeout(()=>{
-    callback(true)  
-    },2000)
-}
-
-
-function ball2(callback){
-    setTimeout(()=>{
-        callback(true)
-    },1000)
-}
-
-
-function ball3(callback){
-    setTimeout(()=>{
-        callback(true)
-    },3000)
-}
-
-
-
-ball1((res)=>{
-    try{
-        if(res){
-            console.log('the ball one reached the end of the hill');
-            ball2((res)=>{
-                try{
-                    if(res){
-                    console.log('the ball two reached the end of the hill');
-                    ball3((res)=>{
-                        try{
-                            if (res){
-                            console.log('the ball three reached the end of the hill');
-                            }
-                            else {
-                                throw new Error("the ball three failed");
-                            }   
-                        }
-                        catch(erorr){
-                            console.log(erorr)
-                        }
-                    })
-            }
-            else{
-                throw new Error("the ball two failed");
-            }}
-                catch(erorr){
-                    console.log(erorr)
-                } 
-        });
-    }
-    else{   
-        throw new Error("the ball one failed");
-    }
-    }
-    catch (error){
-        console.log(error)
-    }
-   
+ball1().then((mes)=>{
+    console.log(mes)
+    return ball2();
+}).then((mes)=>{
+    console.log(mes)
+    return ball3()
+}).then((mes)=>{
+    console.log(mes)
+}).catch((error)=>{
+    console.log(error)
 })
-
