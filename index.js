e5533f0a606844fbec97d20ca2dc8686cab871e5
amir <amirtaki623@gmail.com>
2025-05-09 20:00:46 +0330

    toturail JavaScript Master

--- index.js
+++ index.js
@@ -1,49 +1,96 @@
-document.getElementById("text").innerHTML =  "- Asyncronous : async and await   in JS";
+document.getElementById("0").innerHTML = ` 
+eabcaac  ===>  Asyncronous : async and await   in JS 
+ba364f5  ===>  Asyncronous : Promise   in JS
+0ff8efc  ===>  Managing errors in callbacks in JS
+b6e6273  ===>   Asyncronous CallBack in JS
+d8b8c02  ===>  Asyncronous VS Syncronous in JavaScript
+062f55b  ===>  Babel in javaScript
+95a51b9  ===>   Modules in JavaScript
+4134f3c  ===>   Modules in JavaScript
+5dcf97a  ===>  Redefining the method oop
+d96ce54  ===>  -Inheritance in classes oop
+886433f  ===>  getters and setters for private members oop
+b27b093  ===>   Hide members with WeakMap oop
+2596a20  ===>   Hide members with symbols oop
+930fbd3  ===>  static methods oop in JS
+afe61d3  ===>  Hosting in class oop
+5c5e2a5  ===>  Classes oop in JavaScript
+77c319a  ===>  - Polymorphism oop
+f90de1f  ===>  Method Redefinition oop
+68e8093  ===>  Creating inheritance functions oop
+4aceb59  ===>   Calling the parent constructor oop
+24c9dc2  ===>  Reset Constructor oop
+8ee74d9  ===>  Implementing your own inheritance oop
+ec7da09  ===>   Member Itrate prototype vs instance member oop
+e7d73c3  ===>  prototype vs instance member oop
+6ee27dc  ===>  Constructor Prototype oop
+6bb1f83  ===>  Property description oop
+aec4c56  ===>  Multilevel inheritance oop
+c4ba0b5  ===>   Prototype Inheritance- oop
+61ffa0e  ===>  Inheritance- oop
+5964ac4  ===>  Abstraction- oop in JavaScript
+03e1a65  ===>  Object-oriented programming concepts
+ce48a5f  ===>  What is object-oriented programming?
+19e0c8b  ===>   to change this in  JS
+f5d770c  ===>  Try and Catch in JavaScript
+2924ac6  ===>  Variable definition with VAR
+d5a2e98  ===>  Scope in JavaScript
+eba421e  ===>   Operator Rest
+087d97c  ===>  SET & GET in JavaScript
+88826c2  ===>  Parameter initialization
+4df84c8  ===>  Reduce method in array
+cb25bf3  ===>  Arguments in JS
+45dd6c0  ===>  Hoisting in JavaScript
+c0ba168  ===>  - What 'this' does
+bbaf236  ===>  Array map in JavaScript
+bdc7a24  ===>  Filtering an array
+998bd59  ===>  Testing array elements
+154ba98  ===>  Sorting arrays in JS
+5e17d23  ===>  Array Join in JavaScript
+2d836e2  ===>   Array traversal
+f5ef1de  ===>  Spread operator in votes
+5c11170  ===>  Array concatenation and slicing
+b4e99d3  ===>  Delete all array elementsy
+e14ac2f  ===>  Remove element from array
+7e549dc  ===>  Arrow Function
+356a22d  ===>  Finding an element within an array
+0392e98  ===>  Add element to array
+3fc0ec3  ===>  Object Date in JS
+48d0be5  ===>  template literal in JS
+b19343b  ===>  object string in JS
+5fbd955  ===>  object math in JS
+0a695bf  ===>  garbage collection in JS
+8d60d0e  ===>   Clone an object in JS
+4fc450f  ===>  Navigating the properties of an object
+fb450f4  ===>   - Difference between primitive and reference types
+344050f  ===>  Functions are objects
+8d540f1  ===>  Constructor Property => Construcotr Litral in JS
+efd130a  ===>   - Objects are dynamic in JS
+020f93c  ===>  constructor function
+d098b7a  ===>  factory function in JS
+c5da627  ===>  object OR object Oriented programming in JS
+f2eb417  ===>  break & continue in JS
+358f70a  ===>  LOOP : for in & for out in JS
+2d13078  ===>  do while in JS
+854d5c2  ===>  Loop while in JS
+384996b  ===>  Loop for in JS
+4796709  ===>  switch-case in JS
+e566a99  ===>  Conditional if-else statement
+8bd2bcc  ===>  Operator precedence
+4384082  ===>  -Bitwise operators
+9e5d110  ===>  Logical operators on non-Booleans
+9d9fcb3  ===>  Logical operators in JS
+c024e7d  ===>  Tenary Operator in JS
+321308e  ===>  Compartion Operators in JS
+be98e9a  ===>  Assignment Operators in JS
+c35c904  ===>  Arthmetic Operators
+eb09358  ===>  Refernce Types 3- function type
+0c4fd49  ===>  Refernce Types : 2- Array type
+d69c661  ===>  Referce Types : 1- Object type
+a3d8315  ===>  javaScript is dynamic
+55f8f7d  ===>  primitve (value) types
+eea32fd  ===>  Constants (const) in JS
+319a651  ===>  Variables in JS
+3a75e8b  ===>  folder-oraganization
+`;
 
-/*
-Asyncronous : async and await in JavaScript :
-*/
-
-async function ball1(){
-
-        if(true){
-            return("the ball one reached the end of the hill");
-        }
-        else {
-            throw new Error("the ball one failed");
-        }
-}
-
-async function ball2(){
-
-        if(true){
-            return("the ball two reached the end of the hill");
-        }
-        else {
-            throw new Error("the ball two failed");
-        }
-}
-
-async function ball3(){
-
-        if(true){
-            return("the ball three reached the end of the hill");
-        }
-        else {
-            throw new Error("the ball three failed");
-        }
-}
-
-// console.log(ball1())
-
-async function execute() {
-    try {
-        console.log(await ball1())
-        console.log(await ball2())
-        console.log(await ball3())
-    }catch(e){
-        console.log(e);
-    }
-   
-}
-execute()
