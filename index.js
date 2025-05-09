d8b8c0266f19dce88ab729e2a54c12053d8dbcf5
amir <amirtaki623@gmail.com>
2025-05-09 17:16:43 +0330

    Asyncronous VS Syncronous in JavaScript

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-/* 
-نصب موارد زیر در پوشه
-1- npm init -y
-2- npm i babel-cli babel-core babel-preset-env --save-evn
+document.getElementById("text").innerHTML =  "- Asyncronous VS Syncronous in JS";
+
+/*
+Asyncronous VS Syncronous in JavaScript :
+
+Asynconous :
+1- call back
+2- promise
+3- Async and Await
+
 */
-
-const x = 1;
-
-let numbers = [1, 2, 3, 4, 5, 6]
-numbers.forEach((number)=>{
-    console.log(number)
-})
-
-
-class Circle {
-    
-}
-
-document.getElementById("text").innerHTML =  "- Modules in JS"
